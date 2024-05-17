b1e794e5854e3f65af736a97a209581f2839b73f
Jenya <113167825+Genya10@users.noreply.github.com>
2024-05-17 21:48:22 +0300

    install lucide and create Sidebar(main structure)

---
+++ src/app/components/layout/Sidebar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { Sun, User2, Phone, MessageSquare, Settings } from "lucide-react";
+import cl from "Sidebar.module.scss";
+import Link from "next/link";
+
+export function Sidebar() {
+  return (
+    <div className={cl.sidebar}>
+      <Image src="/linear-icon.svg" alt="" width={50} height={50} />
+      <div>
+        <Link href="/friends">
+          <User2 />
+        </Link>
+        <Link href="/call">
+          <Phone />
+        </Link>
+        <Link href="/chats">
+          <MessageSquare />
+        </Link>
+        <Link href="/settings">
+          <Settings />
+        </Link>
+      </div>
+      <Sun />
+    </div>
+  );
+}
