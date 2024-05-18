65d67836a45612f0eb97832f206550aa077f2d1e
Jenya <113167825+Genya10@users.noreply.github.com>
2024-05-18 20:06:06 +0300

    create LayoutClient and page marking

---
+++ src/app/components/layout/LayoutClient.tsx
@@ -0,0 +1,13 @@
+import type { PropsWithChildren } from 'react';
+import { Sidebar } from './Sidebar';
+import cl from './LayoutClient.module.scss';
+
+export default function LayoutClient({children}:PropsWithChildren<unknown>){
+
+    return (
+           <main className={cl.layout}>
+            <Sidebar/>
+             <section>{children}</section> 
+           </main> 
+    )        
+}
--- src/app/components/layout/Sidebar.tsx
+++ src/app/components/layout/Sidebar.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { Sun, User2, Phone, MessageSquare, Settings } from "lucide-react";
-import cl from "Sidebar.module.scss";
+import cl from "./Sidebar.module.scss";
 import Link from "next/link";
 
 export function Sidebar() {
   return (
-    <div className={cl.sidebar}>
+    <aside className={cl.sidebar}>
       <Image src="/linear-icon.svg" alt="" width={50} height={50} />
       <div>
         <Link href="/friends">
@@ -22,6 +22,6 @@ export function Sidebar() {
         </Link>
       </div>
       <Sun />
-    </div>
+    </aside>
   );
 }
