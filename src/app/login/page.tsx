51aee6bfe4ae680a2fac2b0c43e281d21140c224
Jenya <113167825+Genya10@users.noreply.github.com>
2024-05-23 21:50:05 +0300

    create DB and folder login, seo.constants

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import type {Metadata} from 'next';
+import { NO_INDEX_PAGE } from '../constants/seo.constants';
+
+export const metadata:Metadata ={
+    title:'',
+   ...NO_INDEX_PAGE
+}
+
+export default function LoginPage(){
+    return(
+        <div>
+
+        </div>
+    )
+}
