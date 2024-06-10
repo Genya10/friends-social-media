bb1c1060780293e7fc77ced5f860e4e724aa7265
Jenya <113167825+Genya10@users.noreply.github.com>
2024-06-10 19:48:36 +0300

    create ChatHeader.tsx and add it in Chat.tsx

--- src/app/components/screens/chats/chat/Chat.tsx
+++ src/app/components/screens/chats/chat/Chat.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { $fetch } from "@/api/api.fetch";
+import { useAuth } from "@/app/hooks/useAuth";
 import { IChat } from "@/app/types/chat.types";
 import { useQuery } from "@tanstack/react-query";
 import { Message } from "./Message";
 import { MessageField } from "./MessageField";
+import { ChatHeader } from "./ChatHeader";
+import { Loader } from "lucide-react";
 
 export default function Chat({ id }: { id: string }) {
-  const { data } = useQuery({
+  const { user } = useAuth();
+
+  const { data, isLoading } = useQuery({
     queryKey: ["chat", id],
     queryFn: () =>
       $fetch.get<{ data: IChat }>(
@@ -20,13 +25,22 @@ export default function Chat({ id }: { id: string }) {
     enabled: !!id,
   });
 
+  const correspondent = data?.participants.find((u) => u.email !== user?.email);
+
   return (
     <div className="w-8/12 border-r border-border h-full">
-      <div className="p-layout border-t border-border">
-        {data?.messages.map((message) => (
-          <Message key={message.id} message={message} />
-        ))}
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <ChatHeader correspondent={correspondent} />
+          <div className="p-layout border-t border-border">
+            {data?.messages.map((message) => (
+              <Message key={message.id} message={message} />
+            ))}
+          </div>
+        </>
+      )}
       {/**@ts-ignore */}
       <MessageField />
     </div>
