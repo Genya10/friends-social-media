fe7b5ba1b0be0356c4e526319246e9ec8b3c20cf
Jenya <113167825+Genya10@users.noreply.github.com>
2024-06-17 20:07:22 +0300

    add changes in env and plugins.ts

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,4 +1,4 @@
-module.exports=({env})=>({
+/*module.exports=({env})=>({
 // Настройка плагина strapi-plugin-populate-deep
 "strapi-plugin-populate-deep": {
     config: {
@@ -36,4 +36,49 @@ io: {
         ],
     },
  },
-});
+});*/
+
+module.exports = ({ env }) => ({
+    // Настройка плагина strapi-plugin-populate-deep
+    "strapi-plugin-populate-deep": {
+      config: {
+        // Устанавливает глубину вложенности по умолчанию для популяции данных
+        defaultDepth: 5,
+      },
+    },
+    // Настройка Socket.IO
+    io: {
+      enabled: true, // Включает Socket.IO
+      config: {
+        IOServerOptions: {
+          cors: {
+            origin: "http://localhost:3000", // Разрешает соединения с указанного источника
+            methods: ["GET", "POST"], // Разрешает использование методов GET и POST
+          },
+        },
+        contentTypes: [
+          'api::message.message',//Использование строки вместо объекта для указания типов данных
+          {
+            uid: 'api::chat.chat',
+            actions: ['create'], //Указание массива для действий
+          },
+        ],
+        events: [
+          {
+            // Событие подключения нового клиента
+            name: "connection",
+            // Функция-обработчик для события в Socket.IO.
+            handler: ({ strapi }, socket) => {
+              // Логирует информацию о новом подключении
+              strapi.log.info(`[io] new connection with id ${socket.id}`);
+              // Обработка события 'client-message' от клиента
+              socket.on("client-message", async (messageData) => {
+                // Отправляет полученное сообщение обратно всем клиентам
+                strapi.$io.raw("server-message", messageData);
+              });
+            },
+          },
+        ],
+      },
+    },
+  });
--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,5 @@
-import {Schema, Attribute} from '@strapi/strapi';
+import type { Schema, Attribute } from '@strapi/strapi';
 
-declare module '@strapi/types'{
-    export module Shared {}
+declare module '@strapi/types' {
+  export module Shared {}
 }
